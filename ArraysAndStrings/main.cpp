585260016031a8b7af9f277069993058ee339a62
Vadim P. Minchuk <vadim.p.minchuk@avanade.com>
2018-06-28 11:18:55 -0700

    Added simple IsPermutation function

--- ArraysAndStrings/main.cpp
+++ ArraysAndStrings/main.cpp
@@ -15,8 +15,8 @@ bool StringIsUnique_1(string& str, int& size){
     return true;
 }
 
-// helper array approach, declares boolean array and sets ascii value to true if letter has been seen
-// complexity: O(n)
+// helper array approach, declares boolean array and sets ascii 
+// value to true if letter has been seen complexity: O(n)
 bool StringIsUnique_2(string& str, int& size) {
     bool asciiArr[256] = {false};   // 256 characters in standard ascii table
     for(int i = 0; i < size; i++) {
@@ -27,12 +27,26 @@ bool StringIsUnique_2(string& str, int& size) {
     return true;
 }
 
+// IsPermutation helper function - returns ascii sum of string
+int GetAsciiSum(string& str){
+    int sum = 0;
+    for(char ltr : str)
+        sum += int(ltr);
+    return sum;
+}
+// given 2 strings, checks if one is a permutation of another simple approach 
+// using ascii values, assumes strings are equal length and not the same
+bool IsPermutation(string& str0, string& str1) {
+    return (GetAsciiSum(str0) == GetAsciiSum(str1));
+}
+
 int main() {
     string testStr = "vadim";
+    string testStr_1 = "midav";
     int size = testStr.size();
-    
-    bool result = StringIsUnique_2(testStr, size);
-    (result) ? cout << testStr << " is unique!" : cout << testStr << " is not unique!" << endl;
 
-    // cout << int(testStr[1]);
+    cout << IsPermutation(testStr, testStr_1);
+    
+    // bool result = StringIsUnique_2(testStr, size);
+    // (result) ? cout << testStr << " is unique!" : cout << testStr << " is not unique!" << endl;
 }
